d9f8b09172366c41b3e39a1bdcd12ff15c53460d
Stol_in54 <Stol_in54@student.itstep.org>
2022-07-13 18:11:21 +0200

    qq

--- WinFormsApp8/Profile.Designer.cs
+++ WinFormsApp8/Profile.Designer.cs
@@ -38,9 +38,10 @@
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(294, 347);
+            this.button1.Location = new System.Drawing.Point(336, 463);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(73, 37);
+            this.button1.Size = new System.Drawing.Size(83, 49);
             this.button1.TabIndex = 0;
             this.button1.Text = "Ok";
             this.button1.UseVisualStyleBackColor = false;
@@ -50,28 +51,30 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(20, 33);
+            this.label1.Location = new System.Drawing.Point(23, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.Size = new System.Drawing.Size(66, 23);
             this.label1.TabIndex = 1;
             this.label1.Text = "Name:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(20, 54);
+            this.textBox1.Location = new System.Drawing.Point(23, 72);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(188, 23);
+            this.textBox1.Size = new System.Drawing.Size(214, 27);
             this.textBox1.TabIndex = 2;
             // 
             // Profile
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WinFormsApp8.Properties.Resources.Fon_Login;
-            this.ClientSize = new System.Drawing.Size(379, 396);
+            this.ClientSize = new System.Drawing.Size(449, 528);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Profile";
             this.Text = "Profile";
             this.ResumeLayout(false);
