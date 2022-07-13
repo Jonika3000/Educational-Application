076ea71b26f63295a2bead3f0c7ce78d4f4b577b
Stol_in54 <Stol_in54@student.itstep.org>
2022-07-13 16:45:29 +0200

    Add disp

---
+++ WinFormsApp8/Dispatching.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WinFormsApp8
+{
+    partial class Dispatching
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(33, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Edit schedule";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Dispatching
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Dispatching";
+            this.Text = "Dispatching";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ WinFormsApp8/Dispatching.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp8
+{
+    public partial class Dispatching : Form
+    {
+        public Dispatching()
+        {
+            InitializeComponent();
+            this.BackgroundImage = Image.FromFile("DispatchingFon.jpeg");
+        }
+    }
+}
