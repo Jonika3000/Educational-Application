5d229bf0e7bcba9016c2a958bc23e1b1732beba1
Jonika <katyadyduch@gmail.com>
2022-07-17 17:21:51 +0300

    dddd

--- WinFormsApp8/Add schedule.Designer.cs
+++ WinFormsApp8/Add schedule.Designer.cs
@@ -38,15 +38,16 @@
             this.label4 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(65, 89);
+            this.label1.Location = new System.Drawing.Point(57, 67);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 18);
+            this.label1.Size = new System.Drawing.Size(163, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter name of subject ->";
             // 
@@ -54,9 +55,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(65, 148);
+            this.label2.Location = new System.Drawing.Point(57, 111);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(254, 18);
+            this.label2.Size = new System.Drawing.Size(204, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Enter the time of the lesson ->";
             // 
@@ -64,38 +65,42 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(65, 202);
+            this.label3.Location = new System.Drawing.Point(57, 152);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(256, 18);
+            this.label3.Size = new System.Drawing.Size(205, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "Enter the date of the lesson ->";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(339, 80);
+            this.textBox1.Location = new System.Drawing.Point(297, 60);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.Size = new System.Drawing.Size(110, 23);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(339, 139);
+            this.textBox2.Location = new System.Drawing.Point(297, 104);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.Size = new System.Drawing.Size(110, 23);
             this.textBox2.TabIndex = 4;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(339, 193);
+            this.textBox3.Location = new System.Drawing.Point(297, 145);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.Size = new System.Drawing.Size(110, 23);
             this.textBox3.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(339, 285);
+            this.button1.Location = new System.Drawing.Point(297, 214);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 94);
+            this.button1.Size = new System.Drawing.Size(109, 70);
             this.button1.TabIndex = 6;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
@@ -104,31 +109,41 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(65, 42);
+            this.label4.Location = new System.Drawing.Point(57, 32);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(206, 18);
+            this.label4.Size = new System.Drawing.Size(165, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "Enter name of teacher ->";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(339, 33);
+            this.textBox4.Location = new System.Drawing.Point(297, 25);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.Size = new System.Drawing.Size(110, 23);
             this.textBox4.TabIndex = 8;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(548, 193);
+            this.dateTimePicker1.Location = new System.Drawing.Point(479, 142);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(194, 27);
+            this.dateTimePicker1.Size = new System.Drawing.Size(170, 23);
             this.dateTimePicker1.TabIndex = 9;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(479, 103);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(147, 23);
+            this.maskedTextBox1.TabIndex = 10;
             // 
             // Add_schedule
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
@@ -139,6 +154,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Add_schedule";
             this.Text = "Add_schedule";
             this.ResumeLayout(false);
@@ -158,5 +174,6 @@
         private Label label4;
         private TextBox textBox4;
         private DateTimePicker dateTimePicker1;
+        private MaskedTextBox maskedTextBox1;
     }
 }
