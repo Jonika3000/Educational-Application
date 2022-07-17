2d8717e2681f0cd11a30923d19026f1b1511cada
Stol_in54 <Stol_in54@student.itstep.org>
2022-07-17 18:34:29 +0200

    wewe

--- WinFormsApp8/Marks.Designer.cs
+++ WinFormsApp8/Marks.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(293, 180);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(231, 144);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Marks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Marks";
             this.Text = "Marks";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ListBox listBox1;
     }
 }
